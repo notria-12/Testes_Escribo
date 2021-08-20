19d5c43e8e8b2ddec116b97bc7c19ec78d81c935
TI_02 <airton.araujo@acad.ifma.edu.br>
2021-08-20 08:43:22 -0300

    function finalized

--- main.dart
+++ main.dart
@@ -1,3 +1,13 @@
-void main(List<dynamic> args) {
-  print(args);
+void main(List<String> args) {
+  int sum = 0;
+  int num = int.parse(args[0]);
+
+  if (args.length > 0 && num > 0) {
+    for (var i = 1; i < num; i++) {
+      sum += (i % 3) == 0 || (i % 5) == 0 ? i : 0;
+    }
+    print("RESULTADO: $sum");
+  } else {
+    print('Por favor, informe um número inteiro positivo');
+  }
 }
